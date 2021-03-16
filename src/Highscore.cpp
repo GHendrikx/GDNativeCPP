7e43396cc65139cef0a06946b74cad6ffa841f33
GeoffreyHendrikx <geoffreyhendrikx@hotmail.com>
2021-03-16 14:48:04 +0100

    Push

--- src/Highscore.cpp
+++ src/Highscore.cpp
@@ -8,7 +8,7 @@ Highscore::~Highscore(){}
 
 void Highscore::_register_methods(){
     register_method("_ready", &Highscore::_ready);
-    register_method((char*)"_get_highscore",&Highscore::_get_highscore);
+    //register_method((char*)"_get_highscore",&Highscore::_get_highscore);
 
     register_method((char*)"_on_ReturnButton_pressed", &Highscore::_on_ReturnButton_pressed);
 
@@ -34,6 +34,11 @@ void Highscore::_get_highscore() {
     std::vector<int> scores;
     std::string line;
 
+    for(int i = 0; i<3;i++){
+        //the highscore has everytime 3 places. if you didn't do any game the highscore is 0.
+        scores.push_back(0);
+    }
+
     while (std::getline(myfile, line)) {
         // object from the class stringstream
         std::stringstream geek(line);
@@ -44,12 +49,15 @@ void Highscore::_get_highscore() {
         geek >> x;
         scores.push_back(x);
     }
+
+
+
     std::sort(scores.begin(), scores.end(), std::greater<int>());
 
     for (int i = 1; i < 4; i++) {
         godot::String nodeString = "./Place" + godot::String(std::to_string(i).c_str());
         Label *node = static_cast<Label *>(get_node(NodePath(nodeString)));
-        if (node != NULL) {
+        if (node != nullptr) {
             godot::String number = godot::String(std::to_string(i).c_str());
             godot::String Score = godot::String(std::to_string(scores.at(i - 1)).c_str());
             node->set_text("#" + number + " = " + Score);
